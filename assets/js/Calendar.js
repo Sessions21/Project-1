20d832dc92b5789716ec03b673a6752923de7402
alexvlamakis <alexvlamakis@gmail.com>
2021-12-02 20:57:05 -0700

    avlamakis- start of calendar, time blocks created on js file with todays date format

--- assets/js/Calendar.js
+++ assets/js/Calendar.js
@@ -190,3 +190,26 @@ var displayFiveDay = function (weather) {
 
 //event listener for submission of searched city
 cityFormEl.addEventListener("submit", formSumbitHandler);
+
+// start of calendar
+$(document).ready(function () {
+  // utilizing moment.js file to create the date format reference https://momentjs.com/ to set todays date
+  var today = moment().format('MMMM Do YYYY');
+  console.log(today)
+  $(".todaysDate").append(today);
+
+  var now = parseInt(moment().format('HH'));
+  console.log(now)
+
+      //time blocks created from 9AM to 6PM identifying by ID
+    //read $time9AM-5PM in local storage
+    
+    var $time9AM = $("#time9AM");
+    var $time10AM = $("#time10AM");
+    var $time11AM = $("#time11AM");
+    var $time12PM = $("#time12PM");
+    var $time1PM = $("#time1PM");
+    var $time2PM = $("#time2PM");
+    var $time3PM = $("#time3PM");
+    var $time4PM = $("#time4PM");
+    var $time5PM = $("#time5PM");
