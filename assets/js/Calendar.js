d2b679278f3cf852cf540a080baf265e381d1e87
alexvlamakis <alexvlamakis@gmail.com>
2021-12-02 19:38:39 -0700

    avlamakis- global variables set for weather section on JS file

--- assets/js/Calendar.js
+++ assets/js/Calendar.js
@@ -1,8 +1,21 @@
+//global variables called by ID
+var cities = [];
+//weather/city variables
+var cityFormEl=document.querySelector("#city-search");
+var cityInputEl=document.querySelector("#city")
+var citySearchInputEl = document.querySelector("#searched-city")
+var weatherContainerEl=document.querySelector("#current-weather");
+var forecastTitle = document.querySelector("#forecast");
+var forecastContainerEl = document.querySelector("#five-days");
+
+
 var weatherLink = document.querySelector("#weather")
 
 weatherLink.onClick = () => {
   document.querySelector('[data-target="#weatherModal"]').click();
 };
+
+
 
 
 
