01e460b559797c53d5296586a305b8fced25d58c
Berk Forbes <berkforbes@yahoo.com>
2021-12-05 19:36:01 -0700

    cleaned up functions for weather modal: open and save user input to local storage

--- assets/js/Calendar.js
+++ assets/js/Calendar.js
@@ -1,8 +1,19 @@
+//Variables
 var weatherLink = document.querySelector("#weather")
+var getForecast = document.querySelector('#getForecastBtn')
 
-weatherLink.onClick = () => {
-  document.querySelector('[data-target="#weatherModal"]').click();
-};
+//Weather click in nav bar will show modal
+$(weatherLink).click(function(){
+  $('weatherModal').modal('show')
+})
 
+$(getForecast).on('click', function(){
 
+  $('input[type="text"]').each(function(){    
+      var id = $(this).attr('id');
+      var value = $(this).val();
+     localStorage.setItem(id, value);
+     console.log(id, value);
+  });   
+});
 
