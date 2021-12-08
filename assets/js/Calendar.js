3d206a2932a8f119762500082677bfe5db93d6ad
alexvlamakis <alexvlamakis@gmail.com>
2021-12-07 20:59:00 -0700

    calendar html and weather function

--- assets/js/Calendar.js
+++ assets/js/Calendar.js
@@ -10,6 +10,22 @@ var forecastContainerEl = document.querySelector("#five-days");
 var weatherLink = document.querySelector("#weather")
 var getForecast = document.querySelector('#getForecastBtn')
 
+//Variables
+var weatherLink = document.querySelector("#weather")
+var getForecast = document.querySelector('#getForecastBtn')
+//Weather click in nav bar will show modal
+$(weatherLink).click(function(){
+  $('weatherModal').modal('show')
+})
+$(getForecast).on('click', function(){
+  $('input[type="text"]').each(function(){    
+      var id = $(this).attr('id');
+      var value = $(this).val();
+     localStorage.setItem(id, value);
+     console.log(id, value);
+  });  
+});
+
 
 // Variable targeting the event
 var formSumbitHandler = function (event) {
